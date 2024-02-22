97a42f263c8eed57fda1b175a7706a5dbbf23b4a
arepatiesa <carlosmarcos2004@gmail.com>
2024-02-22 14:19:44 -0400

    Ahora son comandos

--- bot.js
+++ bot.js
@@ -4,25 +4,25 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_KEY)
 bot.start((ctx) => ctx.reply('alo'))
 
-bot.hears('algun pene arepa porai', (ctx) => {
+bot.command('penearepa', (ctx) => {
     ctx.replyWithVideo(Input.fromLocalFile('./media/videos/pene_arepa.mp4'))
 })
-bot.hears('pido replica', (ctx) => {
+bot.command('replica', (ctx) => {
     ctx.reply('han dado una réplica pero qué coño es eso si lo he partido entero le he metido el pollón tieso y he dejado un borbotón de leche tan espeso que voy a regular el calcio que falta en sus huesos')
 })
-bot.hears('llamen a sans', (ctx) => {
+bot.command('alosans', (ctx) => {
     ctx.replyWithVideoNote(Input.fromLocalFile('./media/videos/llamada.mp4'))
 })
 
-bot.hears('alo sans', (ctx) => {
+bot.command('sans', (ctx) => {
     ctx.replyWithVideoNote(Input.fromLocalFile('./media/videos/alosans.mp4'))
 })
 
-bot.hears('algun pelotero arepa porai', (ctx) => {
+bot.command('pelotero', (ctx) => {
     ctx.replyWithPhoto(Input.fromLocalFile('./media/images/pelotero.jpg'))
 })
 
-bot.hears('alguna hembra arepa porai', (ctx) => {
+bot.command('mami', (ctx) => {
     ctx.replyWithPhoto(Input.fromLocalFile('./media/images/mami.jpg'))
 })
 
